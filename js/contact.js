2e999099ac24a76ab5ccb9e5111334fe3f0107e2
VaidyaCharan <charan.n.vaidya@gmail.com>
2023-12-02 10:41:31 +0530

    Changes done on template

--- js/contact.js
+++ js/contact.js
@@ -1,24 +1,24 @@
 function sendMail() {
-    var params = {
-      name: document.getElementById("name").value,
-      email: document.getElementById("email").value,
-      message: document.getElementById("message").value,
-      subject: document.getElementById("subject").value,
-    };
-  
-    const serviceID = "service_jequ20d";
-    const templateID = "template_tukn595";
-  
-      emailjs.send(serviceID, templateID, params)
-      .then(res=>{
-          document.getElementById("name").value = "";
-          document.getElementById("email").value = "";
-          document.getElementById("message").value = "";
-          document.getElementById("subject").value = "";
-          console.log(res);
-          alert("Your message sent successfully!!")
-  
-      })
-      .catch(err=>console.log(err));
-  
-  }
+  var params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+    subject: document.getElementById("subject").value,
+  };
+
+  const serviceID = "service_jequ20d";
+  const templateID = "template_tukn595";
+
+  emailjs.send(serviceID, templateID, params)
+    .then(res => {
+      document.getElementById("name").value = "";
+      document.getElementById("email").value = "";
+      document.getElementById("message").value = "";
+      document.getElementById("subject").value = "";
+      console.log(res);
+      alert("Your message sent successfully!!")
+
+    })
+    .catch(err => console.log(err));
+
+}
